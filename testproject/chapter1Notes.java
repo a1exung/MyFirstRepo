89a6c06806d2ecd9f9a6e0ff8e2f4223594b42cb
a1exung <ajung@stu.naperville203.org>
2022-08-23 12:38:45 -0500

    First uploaded bluej file to repo

---
+++ testproject/chapter1Notes.java
@@ -0,0 +1,38 @@
+/**
+ * Name: Alex Ung
+ * Date: 8/23/22
+ * Chapter 1 Notes
+ * Description: where we are going to take some code notes
+ * Helpers: Mr. Hayes, among others
+ */
+import java.util.Scanner;
+import java.util.Random;
+import java.text.DecimalFormat;
+
+public class chapter1Notes
+{
+    /**
+         * main method
+         * where we are going to code
+         */
+    public static void main (String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        //these are the basic data types
+        int x = 7, y, z;
+        double num = 5.928, num1 = 23.82737, num2;
+        char letter = 'k';
+        boolean isTrue = false;
+        float anum = 8;
+        long a = 9;
+        short b = 5;
+        
+        
+        //These are objects
+        String word = new String("word"); //series of chars
+        Integer number = new Integer(48);
+        Double number2 = new Double(34.928);
+        
+    }
+}
